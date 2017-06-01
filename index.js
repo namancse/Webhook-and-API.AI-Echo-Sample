0cad12814e45ced6ecfa663005ee9e8cc83df88d
namancse <namanjaincse@gmail.com>
2017-06-01 12:27:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,23 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Create bot and add dialogs
+var connector = new builder.ChatConnector({
+    appId: "YourAppId",
+    appPassword: "YourAppSecret"
+});
+var bot = new builder.UniversalBot(connector);  
+bot.dialog('/echo', function (session) {
+    session.send('Hello World');
+});
+
+// Setup Restify Server
+var server = restify.createServer();
+server.post('/api/messages', connector.listen());
+server.listen(process.env.port || 3978, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+});
+/*
 'use strict';
 
 const express = require('express');
@@ -80,3 +100,4 @@ restService.post('/slack-test', function(req, res) {
 restService.listen((process.env.PORT || 8000), function() {
     console.log("Server up and listening");
 });
+*/
